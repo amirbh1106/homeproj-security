a100fdef5dd4e037333770d73dcc9d2ec5127b3f
amirbh1106 <42180486+amirbh1106@users.noreply.github.com>
2021-10-19 20:27:35 +0300

    a
    a

--- new-app/src/components/postcards/card.tsx
+++ new-app/src/components/postcards/card.tsx
@@ -1,11 +1,19 @@
-
+import {useState} from 'react';
 
 
 function Card(props : any){
+    const[clicked, setclicked] = useState<boolean>(false)
     console.log(props.info)
+    function handleclick(){
+        setclicked(!clicked)
+    }
     return(
         <div>
-            <h1>{props.info.title}</h1>
+            <h1 onClick={handleclick}>{props.info.title}{'  >'}</h1> 
+            {clicked && 
+            <div>
+                <a href={props.info.link} style={{fontSize: '2vw'}}>click here for post</a>
+            </div>}
         </div>
     )
     }
